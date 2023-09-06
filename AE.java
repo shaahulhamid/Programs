17cb63024f616cd3e6a6d2278e02e998b9b65244
shaahulhamid <shaahulhamid.kassim@gmail.com>
2023-09-06 15:13:52 +0530

    Programs

---
+++ AE.java
@@ -0,0 +1,11 @@
+class AE
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=0;
+		int res=a/b;
+		System.out.println("Result : "+res);		//Abnormal statements
+		
+	}
+}
---
+++ Driver1.java
@@ -0,0 +1,17 @@
+class A 
+{
+	int a=10;
+}
+class B extends A
+{
+
+}
+class Driver1
+{
+	public static void main(String[] args)
+	{
+		A obj=new A();
+		B obj1=new B();
+		System.out.println(obj1);
+	}
+}
